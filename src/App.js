a61a1d78c3e1fde763d4f70c759ee725e438fbe7
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-05-24 19:23:57 -0700

    Update

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="container btn btn-outline-danger">
-      <button className="btn btn-outline-success">Click me</button>
-    </div>
-  );
-}
